d0f1c4d8a4809d197ea0e06a56f9d7898d126e4b
Isaiah Reed <isaiah.codes@gmail.com>
2016-08-11 17:35:09 -0500

    code cleanup

--- matrixanime.js
+++ matrixanime.js
@@ -1,12 +1,17 @@
 $(document).ready(function(){
 // Clear page function for selection of character
+var xPosition = ($(document).width() / 2) + "px";
+var yPosition = ($(window).height() / 2) + "px";
   var clearPage = function(clickedPic, removedPic1, removedPic2){
-    var xPosition = ($(document).width() / 2) + "px";
-    var yPosition = ($(window).height() / 2) + "px";
-    $('#' + clickedPic).on('click', function(){
-      $('#' + removedPic1).addClass('animated fadeOutUp');
-      $('#' + removedPic2).addClass('animated fadeOutUp').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend',function(){
-        $('.text-background').animate({'left' : xPosition});
+    var clicked = $('#' + clickedPic);
+    var removed1 = $('#' + removedPic1)
+    var removed2 = $('#' + removedPic2)
+    var endAnimateListen = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+      clicked.on('click', function(){
+      removed1.addClass('animated fadeOutUp');
+      removed2.addClass('animated fadeOutUp').one(endAnimateListen, function(){
+          $('.text-background1').animate({'left' : '33%' });
+          $('.text-background3').animate({'right' : '33%' });
         });
       });
     };
@@ -15,7 +20,7 @@ $(document).ready(function(){
 
 //  Whole page fade in --------------------------------
   $('.page-wrapper').fadeIn('slow');
-  // Queue to load other characters
+// Queue to load other characters --------------------
   $('#select-picture-smith').css('display', 'inline-flex').addClass('animated fadeInDown');
   $('#select-picture-smith').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
     $('#select-picture-neo').css('display', 'inline-flex').addClass('animated fadeInDown');
@@ -26,9 +31,8 @@ $(document).ready(function(){
 
   // Animate to quiz on click of character -----------------------------------
   $('#select-picture-smith').click(clearPage('select-picture-smith', 'select-picture-neo', 'select-picture-morpheus'));
-
-  $('#select-picture-neo').on('click', clearPage('select-picture-neo', 'select-picture-smith', 'select-picture-morpheus'));
-  $('#select-picture-morpheus').on('click', clearPage('select-picture-morpheus', 'select-picture-neo', 'select-picture-smith'));
+  $('#select-picture-neo').click(clearPage('select-picture-neo', 'select-picture-smith', 'select-picture-morpheus'));
+  $('#select-picture-morpheus').click(clearPage('select-picture-morpheus', 'select-picture-neo', 'select-picture-smith'));
 
   // Move picture to center of page on click
 
